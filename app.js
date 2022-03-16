360d2b8b240cc9f50e25f7c05dadfc48061f0f87
Sherzod <examle@ggmail.com>
2022-03-16 18:15:05 +0500

    setlnterval

---
+++ app.js
@@ -0,0 +1,60 @@
+// const timer = setInterval(function() {
+//     console.log('Hello word');
+// },2000);
+
+// const timer = setTimeout(
+//     function (text){
+//         console.log(text);
+//     },
+//     2000,
+//     'Hello world'
+// );
+// function logger () {
+//     console.log('Hello world');
+// }
+// const timer = setTimeout(logger,2000);
+
+// clearInterval(timer);
+
+// const btn = document.querySelector('.crimson');
+
+// let timerID;
+// i = 0;
+// btn.addEventListener('click', () => {
+//     // if(i === 3) {
+//     //     clearInterval(timerID);
+//     // }
+//     const timer = setTimeout( function(){
+//         console.log('Hello World');
+//         // i++;
+//         // console.log(i);
+//     },1000);
+// });
+
+
+
+// let id = setTimeout(function  log() {
+//     console.log('Hello');
+//     id = setTimeout(log,1000)
+// })
+
+// const btn = document.querySelector('.crimson');
+// function btnAnimation(){
+//     const box = document.querySelector('.box');
+//     let pos = 0;
+//     const id = setInterval(animationfrom,100);
+//     function animationfrom(){
+//         if(pos === 195){
+//             clearInterval(id);
+//         }else{
+//             pos++;
+//             console.log(pos);
+//             box.style.left = pos + 'px';
+//             box.style.top = pos + 'px';
+//         }
+//     }
+// }
+// btn.addEventListener('click',btnAnimation);
+
+const btn = document.querySelector('.crimson');
+
